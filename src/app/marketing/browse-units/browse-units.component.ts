55332a4cfe3b62ca4fc4db31ea7baf300d3193b4
Shabab Ahmed <52327464+shabab239@users.noreply.github.com>
2024-11-27 16:24:53 +0600

    unit image 360 view

--- src/app/marketing/browse-units/browse-units.component.ts
+++ src/app/marketing/browse-units/browse-units.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormsModule} from "@angular/forms";
 import {CurrencyPipe, NgForOf, NgIf} from "@angular/common";
-import { ActivatedRoute } from '@angular/router';
-import { API_URLS } from '../../util/urls';
+import {ActivatedRoute} from '@angular/router';
+import {API_URLS} from '../../util/urls';
 import {MarketplaceService} from "../service/marketplace.service";
 import {Unit, UnitType, UnitTypeOptions} from "../../construction/project/unit/unit.model";
+import {Viewer} from "@photo-sphere-viewer/core";
 
 @Component({
     selector: 'app-browse-units',
@@ -18,19 +19,25 @@ import {Unit, UnitType, UnitTypeOptions} from "../../construction/project/unit/u
     templateUrl: './browse-units.component.html',
     styleUrl: './browse-units.component.scss'
 })
-export class BrowseUnitsComponent implements OnInit {
+export class BrowseUnitsComponent implements OnInit, OnDestroy {
     API_URLS = API_URLS;
     buildingId: number;
     building: any;
     units: Unit[] = [];
     selectedUnitType: string = '';
     unitTypeOptions = UnitTypeOptions;
+    selectedUnit: Unit | null = null;
+
     loading: boolean = false;
+    showViewer: boolean = false;
+
+    private viewer: Viewer | null = null;
 
     constructor(
         private route: ActivatedRoute,
         private marketplaceService: MarketplaceService
-    ) {}
+    ) {
+    }
 
     ngOnInit() {
         this.route.params.subscribe(params => {
@@ -79,5 +86,50 @@ export class BrowseUnitsComponent implements OnInit {
                 this.loading = false;
             }
         });
+    }
+
+    openViewer(unit: Unit) {
+        this.selectedUnit = unit;
+
+        if (!unit.image) {
+            console.error('No image available for this unit');
+            return;
+        }
+
+        this.showViewer = true;
+
+        // Short delay to ensure modal is visible
+        setTimeout(() => {
+            if (this.viewer) {
+                this.viewer.destroy();
+            }
+
+            this.viewer = new Viewer({
+                container: document.querySelector('#viewer') as HTMLElement,
+                panorama: `${this.API_URLS.image}/unitImages/${unit.image}`,
+                navbar: ['autorotate', 'zoom', 'fullscreen'],
+                defaultZoomLvl: 50,
+                touchmoveTwoFingers: true,
+                mousewheelCtrlKey: true
+            });
+        }, 100);
+    }
+
+    closeViewer() {
+        if (this.viewer) {
+            this.viewer.destroy();
+            this.viewer = null;
+        }
+        this.showViewer = false;
+    }
+
+    ngOnDestroy() {
+        if (this.viewer) {
+            this.viewer.destroy();
+        }
+    }
+
+    contactAgent(unit: Unit): void {
+        console.log('Contact request for unit:', unit);
     }
 }
