52a75dd5baaf56d27658e205f12b36c446b17029
Shabab Ahmed <sabab239.1@gmail.com>
2024-11-30 19:13:11 +0600

    reformatting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,69 +1,11 @@
 import {Routes} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {TasksComponent} from './construction/to-do-list/tasks.component';
-import {EcommercePageComponent} from './pages/ecommerce-page/ecommerce-page.component';
-import {EProductsGridComponent} from './pages/ecommerce-page/e-products-grid/e-products-grid.component';
-import {EProductsListComponent} from './pages/ecommerce-page/e-products-list/e-products-list.component';
-import {EProductDetailsComponent} from './pages/ecommerce-page/e-product-details/e-product-details.component';
-import {ECreateProductComponent} from './pages/ecommerce-page/e-create-product/e-create-product.component';
-import {EEditProductComponent} from './pages/ecommerce-page/e-edit-product/e-edit-product.component';
-import {ECartComponent} from './pages/ecommerce-page/e-cart/e-cart.component';
-import {ECheckoutComponent} from './pages/ecommerce-page/e-checkout/e-checkout.component';
-import {EOrdersComponent} from './pages/ecommerce-page/e-orders/e-orders.component';
-import {EOrderDetailsComponent} from './pages/ecommerce-page/e-order-details/e-order-details.component';
-import {ECreateOrderComponent} from './pages/ecommerce-page/e-create-order/e-create-order.component';
-import {EOrderTrackingComponent} from './pages/ecommerce-page/e-order-tracking/e-order-tracking.component';
-import {ECustomersComponent} from './pages/ecommerce-page/e-customers/e-customers.component';
-import {ECustomerDetailsComponent} from './pages/ecommerce-page/e-customer-details/e-customer-details.component';
-import {ECategoriesComponent} from './pages/ecommerce-page/e-categories/e-categories.component';
-import {ESellersComponent} from './pages/ecommerce-page/e-sellers/e-sellers.component';
-import {ESellerDetailsComponent} from './pages/ecommerce-page/e-seller-details/e-seller-details.component';
-import {ECreateSellerComponent} from './pages/ecommerce-page/e-create-seller/e-create-seller.component';
-import {EReviewsComponent} from './pages/ecommerce-page/e-reviews/e-reviews.component';
-import {ERefundsComponent} from './pages/ecommerce-page/e-refunds/e-refunds.component';
 import {ProjectManagementPageComponent} from './construction/project/project-management-page.component';
 import {ProjectsComponent} from './construction/project/projects/projects.component';
 import {ProjectFormComponent} from './construction/project/project-form/project-form.component';
 import {StagesComponent} from './construction/stage/stages/stages.component';
 import {UsersComponent} from './hr/users/users.component';
-import {LmsPageComponent} from './pages/lms-page/lms-page.component';
-import {LCoursesComponent} from './pages/lms-page/l-courses/l-courses.component';
-import {LCourseDetailsComponent} from './pages/lms-page/l-course-details/l-course-details.component';
-import {LLessonPreviewComponent} from './pages/lms-page/l-lesson-preview/l-lesson-preview.component';
-import {LCreateCourseComponent} from './pages/lms-page/l-create-course/l-create-course.component';
-import {LEditCourseComponent} from './pages/lms-page/l-edit-course/l-edit-course.component';
-import {LInstructorsComponent} from './pages/lms-page/l-instructors/l-instructors.component';
-import {HelpdeskPageComponent} from './pages/helpdesk-page/helpdesk-page.component';
-import {HdTicketsComponent} from './pages/helpdesk-page/hd-tickets/hd-tickets.component';
-import {HdTicketDetailsComponent} from './pages/helpdesk-page/hd-ticket-details/hd-ticket-details.component';
-import {HdAgentsComponent} from './pages/helpdesk-page/hd-agents/hd-agents.component';
-import {HdReportsComponent} from './pages/helpdesk-page/hd-reports/hd-reports.component';
-import {EventsPageComponent} from './pages/events-page/events-page.component';
-import {EventsGridComponent} from './pages/events-page/events-grid/events-grid.component';
-import {EventsListComponent} from './pages/events-page/events-list/events-list.component';
-import {EventDetailsComponent} from './pages/events-page/event-details/event-details.component';
-import {CreateAnEventComponent} from './pages/events-page/create-an-event/create-an-event.component';
-import {EditAnEventComponent} from './pages/events-page/edit-an-event/edit-an-event.component';
-import {SocialPageComponent} from './pages/social-page/social-page.component';
-import {ProfileComponent} from './pages/social-page/profile/profile.component';
-import {TimelineComponent} from './pages/social-page/profile/timeline/timeline.component';
-import {AboutComponent} from './pages/social-page/profile/about/about.component';
-import {ActivityComponent} from './pages/social-page/profile/activity/activity.component';
-import {ProfileSettingsComponent} from './pages/social-page/profile-settings/profile-settings.component';
-import {InvoicesPageComponent} from './pages/invoices-page/invoices-page.component';
-import {InvoicesComponent} from './pages/invoices-page/invoices/invoices.component';
-import {InvoiceDetailsComponent} from './pages/invoices-page/invoice-details/invoice-details.component';
-import {CreateInvoiceComponent} from './pages/invoices-page/create-invoice/create-invoice.component';
-import {EditInvoiceComponent} from './pages/invoices-page/edit-invoice/edit-invoice.component';
-import {UsersPageComponent} from './pages/users-page/users-page.component';
-import {TeamMembersComponent} from './pages/users-page/team-members/team-members.component';
-import {UsersListComponent} from './pages/users-page/users-list/users-list.component';
-import {AddUserComponent} from './pages/users-page/add-user/add-user.component';
-import {StarterComponent} from './starter/starter.component';
-import {ProfilePageComponent} from './pages/profile-page/profile-page.component';
-import {PUserProfileComponent} from './pages/profile-page/p-user-profile/p-user-profile.component';
-import {PTeamsComponent} from './pages/profile-page/p-teams/p-teams.component';
-import {PProjectsComponent} from './pages/profile-page/p-projects/p-projects.component';
 import {AuthenticationComponent} from './authentication/authentication.component';
 import {SignInComponent} from './authentication/sign-in/sign-in.component';
 import {SignUpComponent} from './authentication/sign-up/sign-up.component';
@@ -73,18 +15,6 @@ import {ConfirmEmailComponent} from './authentication/confirm-email/confirm-emai
 import {LockScreenComponent} from './authentication/lock-screen/lock-screen.component';
 import {LogoutComponent} from './authentication/logout/logout.component';
 import {NotFoundComponent} from './not-found/not-found.component';
-import {InternalErrorComponent} from './common/internal-error/internal-error.component';
-import {MapsPageComponent} from './pages/maps-page/maps-page.component';
-import {NotificationsPageComponent} from './pages/notifications-page/notifications-page.component';
-import {MembersPageComponent} from './pages/members-page/members-page.component';
-import {GalleryPageComponent} from './pages/gallery-page/gallery-page.component';
-import {TestimonialsPageComponent} from './pages/testimonials-page/testimonials-page.component';
-import {SearchPageComponent} from './pages/search-page/search-page.component';
-import {ComingSoonComponent} from './coming-soon/coming-soon.component';
-import {BlankPageComponent} from './pages/blank-page/blank-page.component';
-import {PricingPageComponent} from './pages/pricing-page/pricing-page.component';
-import {TimelinePageComponent} from './pages/timeline-page/timeline-page.component';
-import {FaqPageComponent} from './pages/faq-page/faq-page.component';
 import {FrontPagesComponent} from './front-pages/front-pages.component';
 import {HomeComponent} from './front-pages/home/home.component';
 import {AuthGuard} from "./authentication/auth.guard";
@@ -142,31 +72,6 @@ export const routes: Routes = [
             {path: '', component: AdminComponent},
             {path: 'manager', component: ManagerComponent},
             {
-                path: 'ecommerce-page',
-                component: EcommercePageComponent,
-                children: [
-                    {path: '', component: EProductsGridComponent},
-                    {path: 'products-list', component: EProductsListComponent},
-                    {path: 'product-details', component: EProductDetailsComponent},
-                    {path: 'create-product', component: ECreateProductComponent},
-                    {path: 'edit-product', component: EEditProductComponent},
-                    {path: 'orders', component: EOrdersComponent},
-                    {path: 'order-details', component: EOrderDetailsComponent},
-                    {path: 'create-order', component: ECreateOrderComponent},
-                    {path: 'order-tracking', component: EOrderTrackingComponent},
-                    {path: 'customers', component: ECustomersComponent},
-                    {path: 'customer-details', component: ECustomerDetailsComponent},
-                    {path: 'cart', component: ECartComponent},
-                    {path: 'checkout', component: ECheckoutComponent},
-                    {path: 'sellers', component: ESellersComponent},
-                    {path: 'seller-details', component: ESellerDetailsComponent},
-                    {path: 'create-seller', component: ECreateSellerComponent},
-                    {path: 'categories', component: ECategoriesComponent},
-                    {path: 'reviews', component: EReviewsComponent},
-                    {path: 'refunds', component: ERefundsComponent}
-                ]
-            },
-            {
                 path: 'marketing',
                 component: MarketingComponent,
                 children: [
@@ -216,95 +121,6 @@ export const routes: Routes = [
             {path: 'accounts', component: AccountsComponent,},
             {path: 'ledger', component: LedgerHeadsComponent,},
             {path: 'ledger-heads', component: LedgerHeadsComponent,},
-            {
-                path: 'lms-page',
-                component: LmsPageComponent,
-                children: [
-                    {path: '', component: LCoursesComponent},
-                    {path: 'course-details', component: LCourseDetailsComponent},
-                    {path: 'create-course', component: LCreateCourseComponent},
-                    {path: 'edit-course', component: LEditCourseComponent},
-                    {path: 'instructors', component: LInstructorsComponent},
-                    {path: 'lesson-preview', component: LLessonPreviewComponent}
-                ]
-            },
-            {
-                path: 'helpdesk-page',
-                component: HelpdeskPageComponent,
-                children: [
-                    {path: '', component: HdTicketsComponent},
-                    {path: 'ticket-details', component: HdTicketDetailsComponent},
-                    {path: 'agents', component: HdAgentsComponent},
-                    {path: 'reports', component: HdReportsComponent}
-                ]
-            },
-            {
-                path: 'events',
-                component: EventsPageComponent,
-                children: [
-                    {path: '', component: EventsGridComponent},
-                    {path: 'events-list', component: EventsListComponent},
-                    {path: 'event-details', component: EventDetailsComponent},
-                    {path: 'create-an-event', component: CreateAnEventComponent},
-                    {path: 'edit-an-event', component: EditAnEventComponent}
-                ]
-            },
-            {
-                path: 'social',
-                component: SocialPageComponent,
-                children: [
-                    {
-                        path: '',
-                        component: ProfileComponent,
-                        children: [
-                            {path: '', component: TimelineComponent},
-                            {path: 'about', component: AboutComponent},
-                            {path: 'activity', component: ActivityComponent}
-                        ]
-                    },
-                    {path: 'settings', component: ProfileSettingsComponent}
-                ]
-            },
-            {
-                path: 'invoices',
-                component: InvoicesPageComponent,
-                children: [
-                    {path: '', component: InvoicesComponent},
-                    {path: 'invoice-details', component: InvoiceDetailsComponent},
-                    {path: 'create-invoice', component: CreateInvoiceComponent},
-                    {path: 'edit-invoice', component: EditInvoiceComponent}
-                ]
-            },
-            {
-                path: 'users',
-                component: UsersPageComponent,
-                children: [
-                    {path: '', component: UsersComponent},
-                    {path: 'users-list', component: UsersListComponent},
-                    {path: 'add-user', component: AddUserComponent}
-                ]
-            },
-            {
-                path: 'profile',
-                component: ProfilePageComponent,
-                children: [
-                    {path: '', component: PUserProfileComponent},
-                    {path: 'teams', component: PTeamsComponent},
-                    {path: 'projects', component: PProjectsComponent}
-                ]
-            },
-            {path: 'starter', component: StarterComponent},
-            {path: 'timeline', component: TimelinePageComponent},
-            {path: 'pricing', component: PricingPageComponent},
-            {path: 'faq', component: FaqPageComponent},
-            {path: 'gallery', component: GalleryPageComponent},
-            {path: 'testimonials', component: TestimonialsPageComponent},
-            {path: 'search', component: SearchPageComponent},
-            {path: 'blank-page', component: BlankPageComponent},
-            {path: 'internal-error', component: InternalErrorComponent},
-            {path: 'maps', component: MapsPageComponent},
-            {path: 'notifications', component: NotificationsPageComponent},
-            {path: 'members', component: MembersPageComponent},
         ]
     },
     {
@@ -320,8 +136,6 @@ export const routes: Routes = [
             {path: 'logout', component: LogoutComponent}
         ]
     },
-    {path: 'coming-soon', component: ComingSoonComponent},
-    // Here add new pages component
 
-    {path: '**', component: NotFoundComponent} // This line will remain down from the whole pages component list
+    {path: '**', component: NotFoundComponent}
 ];
